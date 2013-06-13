0c76196d9fade836910431b3a31b5d7fc6264fe8
Adam Pearson <adam@substantial.com>
2013-06-13 11:20:35 -0700

    Remove unnecessary package sources that are now in Carton

--- init.el
+++ init.el
@@ -8,11 +8,6 @@
 
 (package-initialize)
 
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-
 (ido-mode t)
 (ido-ubiquitous t)
 
