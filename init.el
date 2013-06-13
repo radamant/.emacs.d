a3077dfdc4a916542717e28c23731d3de278a71e
Adam Pearson <adam@substantial.com>
2013-06-13 11:57:08 -0700

    Extract add-to-lists

--- init.el
+++ init.el
@@ -26,6 +26,7 @@
    my-functions
    my-keybindings
    my-hooks
+   my-add-to-lists
    my-settings
    my-initializers))
 
@@ -34,19 +35,6 @@
         :ignore (.git)))
 
 
-;; load header files in objc-mode
-(add-to-list 'magic-mode-alist
-                `(,(lambda ()
-                     (and (string= (file-name-extension buffer-file-name) "h")
-                          (re-search-forward "@\\<interface\\>"
-                                             magic-mode-regexp-match-limit t)))
-                  . objc-mode))
-
-;; Auto mode list
-(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-(add-to-list 'auto-mode-alist '(".md" . markdown-mode))
 
 (set-face-background 'fringe "#383830")
 (set-face-foreground 'vertical-border "#141411")
---
+++ my/my-add-to-lists.el
@@ -0,0 +1,15 @@
+;; load header files in objc-mode
+(add-to-list 'magic-mode-alist
+                `(,(lambda ()
+                     (and (string= (file-name-extension buffer-file-name) "h")
+                          (re-search-forward "@\\<interface\\>"
+                                             magic-mode-regexp-match-limit t)))
+                  . objc-mode))
+
+;; Auto mode list
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '(".md" . markdown-mode))
+
+(provide 'my-add-to-lists)
