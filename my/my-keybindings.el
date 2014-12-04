80b86ceaa54d7e1d121932627085dfb1d0532375
Adam Pearson <adam@substantial.com>
2014-12-03 17:03:51 -0800

    Miscellaneous updates

--- my/my-keybindings.el
+++ my/my-keybindings.el
@@ -39,6 +39,7 @@
 (global-set-key (kbd "C-c f") 'ff-find-other-file)
 
 (global-set-key (kbd "C-x C-s") 'force-save)
+(global-set-key (kbd "C-x F") 'fill-region)
 (global-set-key (kbd "S-<backspace>") 'pop-to-mark-command)
 
 (provide 'my-keybindings)
